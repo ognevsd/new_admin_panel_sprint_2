cb0911beb5dc694f20109066da9c9dde241c4ea6
Sergey Ognev <ognev.s.d@gmail.com>
2024-07-03 22:24:18 +0300

    Move mixins to separate module

--- docker_compose/app/movies/models.py
+++ docker_compose/app/movies/models.py
@@ -6,24 +6,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
-class TimeStampedMixin(models.Model):
-    # auto_now_add автоматически выставит дату создания записи
-    created = models.DateTimeField(auto_now_add=True)
-    # auto_now изменятся при каждом обновлении записи
-    modified = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class UUIDMixin(models.Model):
-    # Типичная модель в Django использует число в качестве id. В таких
-    # ситуациях поле не описывается в модели.
-    # Вам же придётся явно объявить primary key.
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-    class Meta:
-        abstract = True
+from .mixins import UUIDMixin, TimeStampedMixin
 
 
 class Genre(UUIDMixin, TimeStampedMixin):
