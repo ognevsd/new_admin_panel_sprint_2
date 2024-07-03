592e85893003042bd12c72afc32b9392a6658c3f
Sergey Ognev <ognev.s.d@gmail.com>
2024-07-03 22:24:54 +0300

    Read host and port for app from env

--- docker_compose/app/gunicorn_config.py
+++ docker_compose/app/gunicorn_config.py
@@ -1,1 +1,9 @@
-bind = "0.0.0.0:8000"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_HOST = os.environ.get("APP_HOST")
+APP_PORT = os.environ.get("APP_PORT")
+
+bind = f"{APP_HOST}:{APP_PORT}"
