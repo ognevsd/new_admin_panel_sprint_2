47c476bc9cba102d18910b781e6d116a4281a3de
Sergey Ognev <ognev.s.d@gmail.com>
2024-07-03 22:25:33 +0300

    Apply migrations when building container

--- docker_compose/app/start.sh
+++ docker_compose/app/start.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 python manage.py collectstatic --noinput
 
+while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+done 
+
+python manage.py makemigrations
+python manage.py migrate
+
 gunicorn --config gunicorn_config.py config.wsgi
